42296f01403f5d68fa848f6d5b4cad05ffeeba41
Lil-YH <lamyikhong123@yahoo.com.sg>
2022-09-18 20:15:22 -0400

    Add favicon and logo

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar background">
+          <ul className="nav_list">
+            <div className="App-logo">
+              <img src={require("./tns_pic.png")} alt="logo" />
+            </div>
+
+          </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
